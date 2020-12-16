c47968fdffce7d717af14e12614db4e4281e2025
niccolosantelli <niccolo_santelli@live.it>
2020-12-16 16:02:37 +0100

    struttura principale + rimozione scritte

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,20 @@
+let root = new Vue({
+    el: "#root",
+    data: {
+        frasiPreImpostate: ["esempio 1", "esempio 2", "esempio 3"],
+        frase: "",
+        contenitore: [],
+    },
+    methods: {
+      invioMessaggio(){
+       this.contenitore.push(this.frase)
+     },
+     rimuovi(index){
+       this.$delete(this.contenitore, index)
+     },
+     rimuovi(index){
+       this.$delete(this.frasiPreImpostate, index)
+     },
+   },
 
+});
